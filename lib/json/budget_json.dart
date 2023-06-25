408f178ab7c60bdb3ba5988af22673abfef7dd52
shuklahrs <harshshukla560@gmail.com>
2023-06-25 09:23:20 +0530

    first commit

---
+++ lib/json/budget_json.dart
@@ -0,0 +1,25 @@
+import 'package:budget_tracker_ui/theme/colors.dart';
+
+const List budget_json = [
+  {
+    "name": "Gift",
+    "price": "\Rs2250.00",
+    "label_percentage": "45%",
+    "percentage": 0.45,
+    "color": green
+  },
+  {
+    "name": "Automobile",
+    "price": "\Rs3000.00",
+    "label_percentage": "70%",
+    "percentage": 0.7,
+    "color": red
+  },
+  {
+    "name": "Bank",
+    "price": "\Rs4000.00",
+    "label_percentage": "90%",
+    "percentage": 0.9,
+    "color": blue
+  }
+];
---
+++ lib/json/daily_json.dart
@@ -0,0 +1,32 @@
+const List daily = [
+  {
+    "icon": "assets/images/bank.png",
+    "name": "Bank",
+    "date": "Fri 10AM",
+    "price": "\Rs340.00"
+  },
+  {
+    "icon": "assets/images/auto.png",
+    "name": "Automobile",
+    "date": "Mon 8AM",
+    "price": "\Rs70.00"
+  },
+  {
+    "icon": "assets/images/gift.png",
+    "name": "Gift",
+    "date": "Sat 6PM",
+    "price": "\Rs110.00"
+  },
+  {
+    "icon": "assets/images/eating.png",
+    "name": "Eating",
+    "date": "Sun 5PM",
+    "price": "\Rs60.00"
+  },
+  {
+    "icon": "assets/images/charity.png",
+    "name": "Charity",
+    "date": "Wed 12PM",
+    "price": "\Rs1200.00"
+  }
+];
